9cfc70e165ee5567d4812fb80cabdce061b2b37b
Anoop kumar <therealanoopkumar@gmail.com>
2026-06-04 11:45:20 +0000

    Sync LeetCode submission Runtime - 2 ms (63.79%), Memory - 42.5 MB (96.78%)

---
+++ leetcode/1960-check-if-the-sentence-is-pangram/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        boolean[] arr=new boolean[26];
+        for(int i=0;i<sentence.length();i++){
+            char word=sentence.charAt(i);
+            arr[word - 'a']=true;
+        }
+        for(int i=0;i<arr.length;i++){
+            if (arr[i]==false){
+                return false;
+            }
+        }
+        return true;
+    }
+}
