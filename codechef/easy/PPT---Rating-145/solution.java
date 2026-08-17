e627f93a13ea664eade33c2dcb509f71fcc564e9
Anoop kumar <therealanoopkumar@gmail.com>
2026-08-17 21:19:11 +0530

    ✅ PPT - Rating 145 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ codechef/easy/PPT---Rating-145/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		if (n*30 <= 600) {
+		    System.out.println((600-(n*30))/30);
+		}
+		else {
+		    System.out.println();
+		    
+		}
+		
+
+	}
+}
